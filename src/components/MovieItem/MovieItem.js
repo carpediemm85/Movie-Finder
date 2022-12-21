53e216ab1d57442ae472dd5a3a3f1502d1e0267e
carpediemm85 <rustem.abbaszade@gmail.com>
2022-12-21 11:53:21 +0400

    yes

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -9,7 +9,8 @@ class MovieItem extends Component {
                 <img className="movie-item__poster" src={Poster} alt={Title} />
                 <div className="movie-item__info">
                     <h3 className="movie-item__title">{Title}&nbsp;({Year})</h3>
-                    <button type="button" className="movie-item__add-button" onClick = {() => this.props.addToList(imdbID)}>Add to the list</button>
+                    <button type="button" className="movie-item__add-button"  onClick = {() => this.props.addToList(imdbID)}>Add to the list</button>
+                    
                 </div>
             </article>
         );
